1d0558b3146ae622d7d3d99fab27710bc8847052
Peide Zhong <pzhong@walmartlabs.com>
2015-01-16 11:00:37 -0800

    move observer to static to make sure it is called once

--- src/main/java/com/walmart/labs/pcs/normalize/utils/ObserverExample.java
+++ src/main/java/com/walmart/labs/pcs/normalize/utils/ObserverExample.java
@@ -7,12 +7,15 @@ import java.util.Observer;
  * Created by pzhong1 on 1/15/15.
  */
 public class ObserverExample {
-    public ObserverExample(){
+    static {
         ObservableExample.getObservableExample().addObserver(new Observer() {
             @Override
             public void update(Observable o, Object arg) {
                 System.out.println("observer is called: " + arg);
             }
         });
+    }
+    public ObserverExample(){
+
     }
 }
